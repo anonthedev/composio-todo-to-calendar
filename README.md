3a2f1bf8e979111c141358bd91e047c953764f57
anonthedev <opss59332@gmail.com>
2024-05-20 12:47:51 +0530

    example complete

---
+++ README.md
@@ -0,0 +1,72 @@
+# Todo to Calendar Converter
+
+This Python script allows you to easily convert roughly written todo items into Google Calendar events. It leverages the power of Composio, an open-source command-line tool for developers, to automate the process of creating calendar events based on your todo descriptions.
+
+## Prerequisites
+
+Before you begin, ensure that you have the following installed:
+
+- Python 3.12 or later
+- Git
+
+## Installation
+
+1. **Clone the repository:**
+    ```
+    git clone 
+    ```
+2. **Create a virtual env**
+
+	We've used python 3.12
+
+	``` 
+    python3 -m venv env
+    ```
+	```
+    source env/bin/activate
+    ```
+
+3. **Install required dependencies**
+	 
+	```
+	pip install -r requirements.txt
+	```
+4. **Adding API keys**
+
+	Create a ```.env``` file and add the API keys there.
+    
+    Composio API key can be found here - https://app.composio.dev/settings
+	```
+    OPENAI_API_KEY=sk-
+    COMPOSIO_API_KEY=
+    ```
+
+## Authenticate
+1. **Authenticate composio:**
+	```
+    composio-cli login
+    ```
+2. **Authenticate Google:**
+	
+    Authenticate with the account in which events will be created
+	```
+    composio-cli add googlecalendar
+    ```
+
+## Usage
+
+1. **Edit your todos:**
+
+Open the `main.py` file and edit the `todos` list with your desired todo items in the format `start_time - end_time -> description`. For example:
+
+```python
+todos = '''
+    9AM - 11AM -> Work on project X,
+    1PM - 3PM -> Gaming,
+    # Add more todos here
+'''
+```
+2. **Run the file:**
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import dotenv
+from datetime import datetime
+from crewai import Agent, Task
+from langchain_openai import ChatOpenAI
+from composio_crewai import ComposioToolset, App
+
+dotenv.load_dotenv()
+llm = ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], model_name="gpt-4-turbo-preview")
+
+composio_tool_set = ComposioToolset(apps=[App.GOOGLECALENDAR])
+
+date = datetime.today().strftime('%Y-%m-%d')
+
+timezone = "IST"
+
+todo = '''
+    1PM - 3PM -> Code,
+    5PM - 7PM -> Meeting,
+    9AM - 12AM -> Learn soemthing
+    8PM - 10PM -> Game
+'''
+
+def run_crew():
+    gcal_agent = Agent(role='Google Calendar Agent',
+                       goal="""You take action on Google Calendar using Google Calendar APIs""",
+                       backstory="""You are AI agent that is responsible for taking actions on 
+                                    Google Calendar on users behalf. You need to take action on 
+                                    Calendar using Google Calendar APIs. Use Correct tools to run 
+                                    apis from the given tool-set""",
+                       verbose=True,
+                       tools=composio_tool_set,
+                       llm=llm)
+    task = Task(
+        description=f"book slots according to {todo}. Label them with the work provided to be done in that time period. Schedule it for today. Today's date is {date} (it's in YYYY-MM-DD format) and make the timezone be {timezone}.",
+        agent=gcal_agent,
+        expected_output="if free slot is found"
+    )
+    task.execute()
+    return "Crew run initiated", 200
+
+run_crew()
